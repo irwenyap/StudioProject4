cd42dd168a25ee8dc0d1175fc28f9b2d257a49f3
AorionAce <42329398+AorionAce@users.noreply.github.com>
2020-02-19 19:38:57 +0800

    JonMapT_T

---
+++ Assets/Scripts/GunLogicAi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLogic : MonoBehaviour
+{
+    public Transform player;
+
+
+    public Rigidbody2D bullet;
+
+    // Private
+    float shootBT = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float DistanceAiNPlayer = Vector2.Distance(player.position, this.transform.position);
+        shootBT += Time.deltaTime;
+        if (shootBT >= 0.5f && DistanceAiNPlayer  < 4) {
+            Rigidbody2D rb = Instantiate(bullet, transform.position + (transform.up * 0.5f), transform.rotation);
+            rb.velocity = rb.gameObject.transform.up * 10;
+            shootBT = 0f;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,6 +32,8 @@ public class PlayerController : MonoBehaviour
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
     }
 
     private void FixedUpdate() {
