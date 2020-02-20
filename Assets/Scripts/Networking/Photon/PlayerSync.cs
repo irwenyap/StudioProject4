fd48497c4e622d7d1ed9a980d26297d9571fe582
Krypt <irwenyap2001@gmail.com>
2020-02-20 10:16:55 +0800

    Update PlayerSync with Bullet

--- Assets/Scripts/Networking/Photon/PlayerSync.cs
+++ Assets/Scripts/Networking/Photon/PlayerSync.cs
@@ -7,6 +7,9 @@ public class PlayerSync : MonoBehaviourPun, IPunObservable
 {
     Vector3 latestPos;
     Quaternion latestRot;
+    bool isShooting = false;
+
+    public Rigidbody2D beam;
 
     public MonoBehaviour[] localScripts;
 
@@ -24,17 +27,32 @@ public class PlayerSync : MonoBehaviourPun, IPunObservable
         if (stream.IsWriting) {
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
+            stream.SendNext(isShooting);
         }
         else {
             latestPos = (Vector3)stream.ReceiveNext();
             latestRot = (Quaternion)stream.ReceiveNext();
+            isShooting = (bool)stream.ReceiveNext();
         }
     }
 
     private void Update() {
+
+        if (photonView.IsMine) {
+            if (Input.GetMouseButtonDown(0))
+                isShooting = true;
+            else if (Input.GetMouseButtonUp(0))
+                isShooting = false;
+        }
+
         if (!photonView.IsMine) {
             transform.position = Vector3.Lerp(transform.position, latestPos, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, latestRot, Time.deltaTime * 5);
+        }
+
+        if (isShooting) {
+            Rigidbody2D rb = Instantiate(beam, transform.position + (transform.up * 0.5f), transform.rotation);
+            rb.velocity = rb.gameObject.transform.up * 10;
         }
     }
 }
