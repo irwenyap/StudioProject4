2b3a002875847ef995600c252eed64e75aaaacd4
frostedd1 <42362570+frostedd1@users.noreply.github.com>
2020-02-23 20:42:31 +0800

    New stuff
    -Chest Drop
    -Chest Spawn
    -PowerUp

---
+++ Assets/ChestScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    [SerializeField]
+    CircleCollider2D CCdetection;
+
+    public KeyCode key;
+
+    public GameObject thingInside;
+
+    public LayerMask player;
+
+    private void Awake()
+    {
+        CCdetection = gameObject.GetComponent<CircleCollider2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CCdetection.IsTouchingLayers(player) && Input.GetKeyDown(key))
+        {
+
+            thingInside.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
