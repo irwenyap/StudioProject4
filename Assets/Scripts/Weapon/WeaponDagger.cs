5e3774740225a50fe3d57e280b67ad6eb6b41341
Krypt <irwenyap2001@gmail.com>
2020-02-29 23:27:33 +0800

    Update Dagger Weapon

--- Assets/Scripts/Weapon/WeaponDagger.cs
+++ Assets/Scripts/Weapon/WeaponDagger.cs
@@ -3,64 +3,89 @@ using UnityEngine;
 
 public class WeaponDagger : WeaponBase, IPunObservable {
     public Rigidbody2D projectile;
-    public SpriteRenderer weaponSprite;
 
+    private SpriteRenderer weaponSprite;
     private Transform location;
-    private bool isShooting = false;
-    private float fireRate = 0f;
 
     void Start() {
+        // Disable Photon View on ground to save bandwidth
+        photonView.enabled = false;
+
+        // Reference to component
         weaponSprite = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<BoxCollider2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        // Base Stats
+        attackDamage = 10;
+        attackSpeed = 0.3f;
+        m2Cooldown = 1f;
+        cooldown01 = attackSpeed;
+        cooldown02 = m2Cooldown;
     }
 
     void Update() {
-
         if (isAttached) {
+            cooldown01 += Time.deltaTime;
+            cooldown02 += Time.deltaTime;
+
             // Direction
 
 
             // Shooting
                 if (Input.GetMouseButton(0))
-                    isShooting = true;
-                else
-                    isShooting = false;
+                    isInUseM1 = true;
+                else if (!Input.GetMouseButton(0))
+                    isInUseM1 = false;
+
+                if (Input.GetMouseButton(1))
+                    isInUseM2 = true;
+                else if (!Input.GetMouseButton(1))
+                    isInUseM2 = false;
+
+                if (Input.GetKeyDown(KeyCode.Q))
+                    DropWeapon();
             if (photonView.IsMine) {
+            }
+
+            if (isInUseM1) {
+                var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+                var towards = transform.position + (5 * dir.normalized);
+                transform.position = Vector3.MoveTowards(transform.position, towards, Time.deltaTime);
+                
+                //Rigidbody2D rb = Instantiate(projectile, transform.position, transform.parent.rotation);
+                //rb.velocity = rb.gameObject.transform.up * 10;
+                //fireRate = 0f;
             }
         }
 
-        //fireRate += Time.deltaTime;
-
-        if (isShooting) {
-            var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-            var towards = transform.position + (5 * dir.normalized);
-            transform.position = Vector3.Lerp(transform.position, dir.normalized, 5 * Time.deltaTime);
-
-            //Rigidbody2D rb = Instantiate(projectile, transform.position, transform.parent.rotation);
-            //rb.velocity = rb.gameObject.transform.up * 10;
-            //fireRate = 0f;
-        }
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
         if (stream.IsWriting) {
-            stream.SendNext(isShooting);
+            stream.SendNext(isInUseM1);
+            stream.SendNext(isInUseM2);
         }
         else {
-            isShooting = (bool)stream.ReceiveNext();
+            isInUseM1 = (bool)stream.ReceiveNext();
+            isInUseM2 = (bool)stream.ReceiveNext();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (!collision.GetComponent<PlayerController>().weaponIsOnHand) {
-            collision.GetComponent<PlayerController>().weaponIsOnHand = true;
-            location = collision.transform.Find("Weapon");
-            Destroy(GetComponent<Rigidbody2D>());
-            GetComponent<BoxCollider2D>().enabled = false;
+            myPlayer = collision.GetComponent<PlayerController>();
+            myPlayer.weaponIsOnHand = true;
             transform.SetParent(collision.transform);
+            WeaponOnHand(true);
+
+            // Setting the item's transform when attached
             transform.localPosition = new Vector3(0.2f, -0.15f, -1);
-            transform.localScale = new Vector3(0.3f, 0.3f, 1);
+            transform.localScale = new Vector3(5, 5, 1);
             transform.localRotation = Quaternion.Euler(0, 0, 45);
-            isAttached = true;
+
+            // Photon ownership transfer on pickup
+            photonView.enabled = true;
             photonView.TransferOwnership(collision.GetComponent<PhotonView>().Owner);
         }
     }
