48243f437901e3e56c3ac2abe44dd2e4c4b62bc3
AorionAce <42329398+AorionAce@users.noreply.github.com>
2020-03-04 14:48:06 +0800

    FireBoss

---
+++ Assets/Scripts/AirProjectileOnHit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirProjectileOnHit : AirBoss
+{
+    public Transform player;
+    public AirBoss airboss;
+    private int damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        airboss = GameObject.FindGameObjectWithTag("Airboss").GetComponentInChildren<AirBoss>();
+        damage = airboss.damage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+            collision.gameObject.GetComponent<PlayerController>().TakeDamage(damage);
+            Destroy(gameObject);
+
+        }
+    }
+    
+}
