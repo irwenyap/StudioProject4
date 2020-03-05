56690e79a7b4e866cf5ece75eb5d740edfb8af86
Krypt <irwenyap2001@gmail.com>
2020-03-05 11:48:22 +0800

    Added PlaySound

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySound : MonoBehaviour {
+    public AudioClip sound;
+
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+
+    // Start is called before the first frame update
+    void Start() {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+        button.onClick.AddListener(ApplySound);
+    }
+
+    void ApplySound() {
+        source.PlayOneShot(sound);
+    }
+}
